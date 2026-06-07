3e08e4640c88e66cf6a656879011d1a6cd238a9d
melvinchia3636 <melvinchia623600@gmail.com>
2026-06-07 21:47:30 +0800

    fix: fix overflow issue on desktop print queue

--- client/src/components/PrintQueue/views/desktop.tsx
+++ client/src/components/PrintQueue/views/desktop.tsx
@@ -3,7 +3,7 @@ import QueueList from "../components/QueueList";
 export default function PrintQueueDesktopView() {
   return (
     <aside className="hidden lg:flex w-80 shrink-0">
-      <div className="card bg-base-200 shadow-sm flex-1 min-w-0">
+      <div className="card overflow-y-auto bg-base-200 shadow-sm flex-1 min-w-0">
         <QueueList />
       </div>
     </aside>
